c6b7442ac72c665ec7a840effff933641d347c9a
Eser-k <eser.kayali@adesso.de>
2024-12-16 16:20:03 +0100

    Implemented a RESTAPI Demo

---
+++ RESTAPIs/src/main/java/com/example/RESTAPIs/rest/StudentRestController.java
@@ -0,0 +1,41 @@
+package com.example.RESTAPIs.rest;
+
+
+import com.example.RESTAPIs.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+        students.add(new Student("Naruto","Uzumaki"));
+        students.add(new Student("Son","Goku"));
+        students.add(new Student("Senku","Ishigami"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        if( (studentId >= students.size()) || (studentId < 0) ){
+            throw new StudentNotFoundException("Student not found - " + studentId);
+        }
+        return students.get(studentId);
+    }
+
+}
